55b482f70359975e10b8de511b43110e7cef5090
Abdul Rahman <abdul.rahman@bbnl.co.in>
2025-05-29 01:10:28 +0530

    [docker, node version 20] upgraded

--- feapp/src/pages/Product.jsx
+++ feapp/src/pages/Product.jsx
@@ -1,5 +1,17 @@
+import axios from "axios";
+import {useQuery} from "@tanstack/react-query";
+import Link from "react-router-dom";
+
+const fetchPost = async ()=>{
+ const res = await axios.get('https://jsonplaceholder.typicode.com/todos/');
+ return res.data;
+};
+
 const Product = ()=>{
-  return (
+   const { data, isLoading, error } = useQuery(['posts'], fetchPost);
+   if(isLoading) return <p>Loading posts....</p>;
+   if(error) return <p>Error fetching posts</p>;
+   return (
 	<div>Test</div>
   );
 };
